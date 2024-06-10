1c3f15f4ca4e7580259b43d3f60a1ae4531b8cbe
Lorys Lellouche <lorys.lellouche@gmail.com>
2024-06-10 21:35:35 +0200

    exo 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,27 @@
+import "./PokemonCard.module.css";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const PokemonCard = () => {
+  const pokemon = pokemonList[0];
+  console.log(pokemon.name);
   return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt=""
-      />
-      <figcaption>Bulbasaur</figcaption>
+    <figure className="card">
+      {pokemon.imgSrc ? (
+        <img className="card-img" src={pokemon.imgSrc} alt="" />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 };
